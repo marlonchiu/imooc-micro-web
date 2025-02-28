1e6af656247f4ad54867725f64253f48f43a1cf2
marlonchiu <Knight1898@outlook.com>
2025-02-28 16:13:20 +0800

    4-1 vue2子应用-新能源页面

---
+++ vue2/src/views/energy/components/adBanner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ad-banner-container">
+    <img v-if="adImg" :src="adImg" alt="" />
+    <img v-else src="../../../assets/ad.png" alt="" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'adBanner',
+  props: {
+    adImg: { type: String, default: '' }
+  }
+}
+</script>
+
+<style lang="scss">
+.ad-banner-container {
+  width: 100%;
+  height: 80px;
+  margin: 30px 0;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
