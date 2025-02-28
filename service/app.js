e2f3d97342a3f7e6df634e01688f8d17bc53327c
marlonchiu <Knight1898@outlook.com>
2025-02-28 22:04:34 +0800

    4-10 后端服务请求处理

--- service/app.js
+++ service/app.js
@@ -10,22 +10,29 @@ const cors = require('koa2-cors')
 const index = require('./routes/index')
 const users = require('./routes/users')
 const vue2 = require('./routes/vue2')
+const vue3 = require('./routes/vue3')
+const react15 = require('./routes/react15')
+const react16 = require('./routes/react16')
 
 // error handler
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text']
+  })
+)
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 app.use(cors())
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + '/views', {
+    extension: 'pug'
+  })
+)
 
 // logger
 app.use(async (ctx, next) => {
@@ -39,10 +46,13 @@ app.use(async (ctx, next) => {
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 app.use(vue2.routes(), vue2.allowedMethods())
+app.use(vue3.routes(), vue3.allowedMethods())
+app.use(react15.routes(), react15.allowedMethods())
+app.use(react16.routes(), react16.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+})
 
 module.exports = app
